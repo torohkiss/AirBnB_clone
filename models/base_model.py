33eef130c2d405c1647bc1eb9cfe064e64e2b236
torohkiss <torohkiss@yahoo.com>
2024-05-29 04:26:30 -0700

    task 3c

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,12 @@ import models
 
 class BaseModel:
     """The Basemodel class"""
+
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+
     def __str__(self):
         print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
