aef65998154cd86e4f040a0d83f3a2edabcd45d3
torohkiss <torohkiss@yahoo.com>
2024-05-29 06:17:42 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
