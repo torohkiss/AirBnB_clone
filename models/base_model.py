f48dceb15794098d8620db99dc08fdbdef070584
torohkiss <torohkiss@yahoo.com>
2024-05-31 05:29:32 -0700

    task 5r

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """The base model class"""
 import uuid
-import datetime
+from datetime import datetime
 import models
 #from model import storage
 
@@ -24,20 +24,20 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         models.storage.new(self)
 
     def __str__(self):
         print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        adict = {key: value for key, value in self.__dict__.items()}
-        #adict = self.__dict__.copy()
+        #adict = {key: value for key, value in self.__dict__.items()}
+        adict = self.__dict__.copy()
         adict["__class__"] = self.__class__.__name__
         adict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         adict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
