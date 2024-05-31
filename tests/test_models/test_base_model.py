26ef71122ff0a9cdbd0acbff7d982bfbcfe24692
torohkiss <torohkiss@yahoo.com>
2024-05-31 06:40:06 -0700

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """Test class for BaseModel"""
-import models
-import uuid
+import json
 import unittest
+import uuid
 import os
-import sys
-Base = models.base_model.BaseModel
+from datetime import datetime
+from models.base_model import BaseModel
 
 
 class TestBestModel(unittest.TestCase):
     """TestBaseMidelClass"""
 
     def setUp(self):
-        self.base1 = Base()
-        self.base2 = Base()
+        self.base1 = BaseModel()
+        self.base2 = BaseModel()
 
     def tearDown(self):
-        self.base1 = None
-        self.base2 = None
+        del self.base1
+        del self.base2
 
-    def testBaseModel(self):
+    def test_BaseModel(self):
         the_id = str(uuid.uuid4())
         self.assertIsInstance(the_id, str)
         self.assertTrue(len(the_id), 36)
+
+
 
 
 if __name__ == "__main__":
