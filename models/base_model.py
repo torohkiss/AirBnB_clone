2dad102a7cbfb53f72a9964e308890a277e2dbaf
torohkiss <torohkiss@gmail.com>
2025-03-15 00:19:29 +0100

    task base model

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,10 @@ class BaseModel:
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
-                if key != __class__:
-                    value = datetime.strptime(value, time_format)
-                setattr(self, key, value)
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, time_format)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
