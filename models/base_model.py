263300e2c87abcdd3c51686a8ddc3d623f82ff1b
torohkiss <torohkiss@gmail.com>
2025-03-15 00:10:49 +0100

    test base model again

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,12 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                self.__setattr__(key, value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+                if key != __class__:
+                    self.__setattr__(key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
         
     def to_dict(self):
         new_dict = self.__dict__.copy()
