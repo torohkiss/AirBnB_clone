cc036e50cd8bde8956716b0b5607c3bd8fccceb4
torohkiss <torohkiss@yahoo.com>
2025-03-15 16:24:38 +0000

    reformated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -12,21 +12,25 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                self.__setattr__(key, value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+                if key == '__class__':
+                    continue
+                if key in ('created_at', 'updated_at'):
+                    value = datetime.toisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+         self.updated_at = datetime.now()
         
     def to_dict(self):
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        new_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return new_dict
-
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-    def save(self):
-        self.updated_at = datetime.now()
