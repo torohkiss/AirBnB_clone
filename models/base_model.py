d64e31dd62d17cf732a8650cf50cd6cca2767fca
torohkiss <torohkiss@gmail.com>
2025-03-15 12:45:22 +0100

    regigged base_model

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,11 @@ class BaseModel:
     """The BaseModel class with common attributes/methods"""
 
     def __init__(self, *args, **kwargs):
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, time_format)
+                        value = datetime.fromisoformat(value)
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -33,6 +32,6 @@ class BaseModel:
         """returns dictionary representation"""
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        new_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return new_dict
