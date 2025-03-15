c3092ba1cf5282d27ff230580caf5934ee3da9bd
torohkiss <torohkiss@yahoo.com>
2025-03-15 19:16:18 +0000

    the console module

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """The console comand interpreter"""
+
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """quits the console"""
+        return True
+
+    def do_quit(self, line):
+        """also quits the console"""
+        return True
+
+    def emptyline(self):
+        """an emptyline that does nothing"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
